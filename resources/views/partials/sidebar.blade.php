f32e25025402170163ae6cd83439744eb4d5748f
KINN05 <rifqialkindi57@gmail.com>
2026-05-21 21:51:38 +0700

    Update sidebar.blade.php

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -5,7 +5,7 @@
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-school"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">SISWA</div>
+        <div class="sidebar-brand-text mx-3">SIMSIS</div>
     </a>
 
     <hr class="sidebar-divider my-0">
