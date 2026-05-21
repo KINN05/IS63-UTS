320da8adec2840027c7a465d6c3cb2851632842e
KINN05 <rifqialkindi57@gmail.com>
2026-05-21 21:53:23 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>@yield('title', 'Dashboard') - Siswa</title>
+    <title>@yield('title', 'Dashboard') - SIMSIS</title>
 
     {{-- Google Fonts --}}
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,300,400,600,700,800,900" rel="stylesheet">
