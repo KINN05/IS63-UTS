15d5a2fc358010f7794207496b2b6f9eee1306ee
KINN05 <rifqialkindi57@gmail.com>
2026-05-21 22:00:31 +0700

    Membuat Partial Footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+{{-- resources/views/partials/footer.blade.php --}}
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>
+                &copy; {{ date('Y') }} <strong>SIMSIS</strong>
+                &mdash; Sistem Informasi Manajemen Siswa
+            </span>
+        </div>
+    </div>
+</footer>
